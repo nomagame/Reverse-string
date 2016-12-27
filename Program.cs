b1ab8f5deb4695f1673732a548ed613e2b9b6418
Noma <1181098979@qq.com>
2016-12-27 21:46:24 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Application
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string input = Convert.ToString(Console.ReadLine());
+
+			char[] cha = input.ToCharArray();
+
+			for (int i = input.Length -1; i >= 0; i--)
+			{
+				Console.Write(cha[i]);
+			}
+		}
+
+	}
+}
